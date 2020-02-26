899c999cd56234975418e0816afe3e9d7cb68433
Love Ottenby <love@Alex.mediegruppen.se>
2020-02-26 14:07:44 +0100

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+// Härifrån startar vi upp vår webshop
+const mongoose = require('mongoose')
+const dbUrl = process.env.MONGO_ATLAS_URL
+const { app, port } = require('./src/server')
+
+// Kicka igång servern
+const dbOptions = { useUnifiedTopology: true, useNewUrlParser: true }
+mongoose.connect(dbUrl, dbOptions).then(() => {
+    app.listen(port, () => console.log(`App listening on port ${port}!`))
+})
+
+module.exports = { app, port }
